3ea0304b62e0f88dd658038560d4bd9a14d93038
akm <akm2000@gmail.com>
2016-02-26 17:01:46 +0900

    Displaying heroes in a template

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,12 @@ interface Hero {
     selector: 'my-app',
     template:`
       <h1>{{title}}</h1>
+      <h2>My Heroes</h2>
+      <ul class="heroes">
+        <li>
+          <!-- each hero goes here -->
+        </li>
+      </ul>
       <h2>{{hero.name}} details!</h2>
       <div><label>id: </label>{{hero.id}}</div>
       <div>
